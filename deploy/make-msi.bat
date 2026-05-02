d848f38e5349de1f930834c16b7d48f7276b93c7
Jonas Kümmerlin <jonas@kuemmerlin.eu>
2026-05-02 22:18:05 +0200

    msi generator: fix path to ico
    forgot to change this during the icon move

--- deploy/make-msi.bat
+++ deploy/make-msi.bat
@@ -1,3 +1,3 @@
 call "%~dp0..\version.inc.bat"
 
-"%programfiles(x86)%\jkmsigen\jkmsigen.exe" --upgrade-code=cfb86682-eb9c-4773-983f-6afb572d5b0a --name "FishyRecorder Lite" --version "%KUEMMELRECORDER_VERSION%" --output-msi "%~dp0out\FishyRecorder-%KUEMMELRECORDER_VERSION%.msi" "%~dp0bindir" --shortcut "KuemmelRecorder-Lite.exe" --icon "%~dp0..\img\fishy.ico" --x64 --installdir "FishyRecorder Lite" --with-ui "en-us"
+"%programfiles(x86)%\jkmsigen\jkmsigen.exe" --upgrade-code=cfb86682-eb9c-4773-983f-6afb572d5b0a --name "FishyRecorder Lite" --version "%KUEMMELRECORDER_VERSION%" --output-msi "%~dp0out\FishyRecorder-%KUEMMELRECORDER_VERSION%.msi" "%~dp0bindir" --shortcut "KuemmelRecorder-Lite.exe" --icon "%~dp0..\icons\fishy.ico" --x64 --installdir "FishyRecorder Lite" --with-ui "en-us"
